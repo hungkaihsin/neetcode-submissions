c96cbd8441a21f043752c5a97785f783e7758f72
Kai-Hsin, Hung <66995361+hungkaihsin@users.noreply.github.com>
2026-05-08 09:16:57 -0700

    Add: anagram-groups - submission-11

---
+++ Data Structures & Algorithms/anagram-groups/submission-11.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
+
+# Time O(m * n) m = length of the array, n = average length of array
+# Space O(m) the number of unique word
