6ba9522758dcf34da9ed1b261c45dddb49cf9011
Kai-Hsin, Hung <66995361+hungkaihsin@users.noreply.github.com>
2026-07-12 14:30:39 +0800

    Add: two-integer-sum - submission-21

---
+++ Data Structures & Algorithms/two-integer-sum/submission-21.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        preMap = {} # val -> index
+
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in preMap:
+                return [preMap[diff], i]
+            preMap[num] = i
+
+# time O(n) interate the input arrary
+# space O(n) dict (hashmap)
