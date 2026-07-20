77ca85f992f4c8c0418d88ea412f0bcba901b230
Kai-Hsin, Hung <66995361+hungkaihsin@users.noreply.github.com>
2026-07-20 15:17:29 +0800

    Add: is-anagram - submission-30

---
+++ Data Structures & Algorithms/is-anagram/submission-30.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
+
+
+# time O(m + n) m = countS, n = countT
+# space O(1) only 26 chars the most
